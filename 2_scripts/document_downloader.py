af075de111d570a4c0fbaa905aed257443de98a1
Retep-dev <afolabiengopet@gmail.com>
2026-05-08 12:24:16 +0100

    More Kubernetes(10)

--- 2_scripts/document_downloader.py
+++ 2_scripts/document_downloader.py
@@ -134,6 +134,6 @@ def download_document(url: str, category: str = None, filename: str = None):
 # ▶️ RUN SCRIPT
 # =========================
 if __name__ == "__main__":
-    url = "https://kubernetes.io/docs/concepts/storage/persistent-volumes/"
+    url = "https://kubernetes.io/docs/concepts/storage/volume-snapshots/"
 
     download_document(url, category="kubernetes")
