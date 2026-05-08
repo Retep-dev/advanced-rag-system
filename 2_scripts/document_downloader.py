395b31783dda6da677bca1d16774242243ab8a69
Retep-dev <afolabiengopet@gmail.com>
2026-05-08 14:04:36 +0100

    Docker (1)

--- 2_scripts/document_downloader.py
+++ 2_scripts/document_downloader.py
@@ -134,6 +134,6 @@ def download_document(url: str, category: str = None, filename: str = None):
 # ▶️ RUN SCRIPT
 # =========================
 if __name__ == "__main__":
-    url = "https://kubernetes.io/docs/concepts/storage/volume-attributes-classes/"
+    url = "https://docs.docker.com/engine/install/fedora/"
 
-    download_document(url, category="kubernetes")
+    download_document(url, category="docker")
