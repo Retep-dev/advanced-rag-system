d6411e676a4e17db7d57b116fd5509d5cdc54f07
Retep-dev <afolabiengopet@gmail.com>
2026-05-13 13:36:21 +0100

    Docker (4)

--- 2_scripts/document_downloader.py
+++ 2_scripts/document_downloader.py
@@ -134,6 +134,6 @@ def download_document(url: str, category: str = None, filename: str = None):
 # ▶️ RUN SCRIPT
 # =========================
 if __name__ == "__main__":
-    url = "https://docs.docker.com/engine/network/firewall-iptables/"
+    url = "https://docs.docker.com/engine/network/drivers/macvlan/"
 
     download_document(url, category="docker")
